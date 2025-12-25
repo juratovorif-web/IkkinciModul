c19397506bef82f5e3789f39f0ddb37025342fbc
juratovorif-web <juratovorif@gmail.com>
2025-12-25 23:49:24 +0500

    uwa gap

--- Trening/Trening1/Program.cs
+++ Trening/Trening1/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student = new Student();
+
+            student.StudentId = Guid.NewGuid();
+            student.FirstName = "Jon";
+            student.LastName = "Ly";
+            student.Phone = "+998887778877";
+            student.Email = "li@gmail.com";
+            student.Address = "Uzbekistan";
+
+            student.PrintAllInfo();
+            Console.WriteLine(student.Address);
+
+            student.ToString();
+            Console.WriteLine(student);
         }
     }
 }
---
+++ Trening/Trening1/Students.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Trening1;
+
+internal class Student
+{
+
+    public Guid StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    
+
+    public void PrintAllInfo()
+    {
+        Console.WriteLine($"ID {StudentId}");
+        Console.WriteLine($"Firs {FirstName} and last {LastName}");
+        Console.WriteLine($"Phone {Phone}");
+        Console.WriteLine($"Email{Email}");
+    }
+    override public string ToString()
+    {
+        return $"ID {StudentId}\n FirstName {FirstName}\n Last {LastName}\n Email {Email}\n Phone {Phone}\n Adress{Address}";
+    }
+
+
+
+
+}
